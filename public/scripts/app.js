87aa13ac097a58876bebe3b7711ee4fd3e137717
Gaganpandher52 <gaganpandher52@gmail.com>
2019-05-16 21:06:56 +0000

    compose button installed and working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,8 @@ $( document ).ready(function() {
 
 function createTweetElement(data){
     //const content = data.content.text;
+
+    const $button = $('<$button>').addClass('compose-button');
     const $tweet = $('<article>').addClass('tweet')
     const $header = $('<header>').addClass('all-headers');
     
@@ -24,6 +26,7 @@ function createTweetElement(data){
     .addClass('newFooter');
     const $hr = $('<hr>');
     
+    $button.append($button)
     $header.append($avatar).append($name).append($handle);
 
     return $tweet.append($header).append($content).append($hr).append($footer);
@@ -67,6 +70,19 @@ function renderTweets(tweets) {
       }
   }
 
+$( ".compose-button" ).click(function() {
+  $( ".new-tweet" ).slideToggle( "slow", function() {
+    $( "textarea" ).focus();
+
+    // Animation complete.
+  
+  });
+});
+
+
+
+
+
 //function isValidate()
 
 function clearTweet(){
@@ -76,10 +92,14 @@ function clearTweet(){
 $(".tweet-form").submit(function(event){
     event.preventDefault();
     if(!$('textarea').val()){
-      console.log('noooooo');
+      $( ".error-empty" ).slideDown( "slow", function() {
+        $( ".error-empty" ).slideUp();
+      });
       const $name = $('<p>').text('empty tweet')
     }else if($('textarea').val().length > 140){
-      alert('more than 140');
+      $( ".error-140" ).slideDown( "slow", function() {
+
+      });
     }else{
     
     var $form = $(this);
