bcaf7911fc6756c4af734583c46c5bc856eea92a
Gaganpandher52 <gaganpandher52@gmail.com>
2019-05-16 02:45:58 +0000

    all tweets rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,54 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$( document ).ready(function() {
 
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
 function createTweetElement(data){
     //const content = data.content.text;
@@ -72,19 +27,6 @@ function createTweetElement(data){
     $header.append($avatar).append($name).append($handle);
 
     return $tweet.append($header).append($content).append($hr).append($footer);
-
-    // const tweet = `
-    // <article class="tweet newArticle" >
-    //     <header class="all-headers">
-    //         <div class="inner">${data.user.name}</div>
-    //         <div class="image"><img  src="https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png" alt=""></div>
-    //         <div class="user-tag">@Mrfields</div>
-    //     </header>
-    //     <p  id="hello" class="tweet">Now that you have the regular tweet</p>
-    //     <hr>
-    //     <footer class="newFooter">10 days ago</footer>
-    // </article>
-    // `
 }
 function formatTime (time) {
 	var diff = Math.floor((Date.now() - time) / 1000);
@@ -119,11 +61,54 @@ function renderTweets(tweets) {
     // loops through tweets
       // calls createTweetElement for each tweet
       // takes return value and appends it to the tweets container
-      let value = [];
+      //let value = [];
       for (const i in tweets) {
-        value.push(createTweetElement(tweets[i]));
-        
+        // value.push(createTweetElement(tweets[i]));
+        $('.tweets-container').prepend(createTweetElement(tweets[i]))
       }
-      return $('.tweets-container').append(value);
+      //return $('.tweets-container').append(value);
   }
-renderTweets(data);
+//renderTweets(data);
+
+function clearTweet(){
+    $('textarea').val('');
+}
+
+$(".tweet-form").submit(function(event){
+    event.preventDefault();
+
+    var $form = $(this);
+    let term = $form.find("textarea[name='text']").val();
+    //let url = $form.attr("action");
+    
+    $.post('/tweets', {text:term}).done(function(){
+        console.log(term);
+        loadedTweets();
+        clearTweet();
+    });
+  });//form submit
+
+  function loadedTweets(){
+    $.ajax({url:'/tweets',  method: 'GET' })
+    .then(function (tweets) {
+      //$('.tweet-container')
+      renderTweets(tweets);
+    });
+}
+
+renderTweets(loadedTweets());
+
+
+
+
+
+
+   
+    
+      
+
+
+
+
+
+});//document
